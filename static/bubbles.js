c9eb7bc28199819e68dbc909e912b5aee731f689
Wilson Berkow <wilsonberkow@gmail.com>
2017-03-31 13:46:48 -0400

    Create initial D3 bubbles (using random data)

--- static/bubbles.js
+++ static/bubbles.js
@@ -2,18 +2,22 @@
     "use strict";
     // Use http://www.w3.org/2000/svg for svg elements
 
-    var special = d3.select("slate-special");
+    var special = d3.select("#slate-special");
 
     var data = [Math.random() * 100];
     for (let i = 1; i < 9; i += 1) {
 	data[i] = data[i - 1] + Math.random() * 100;
     }
 
-    var bubbles = special.selectAll("circle");
-    bubbles.data(data);
-    bubbles.enter().append(function (d) {
-	return document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    }).style("r", function (area) {
-	return "" + Math.sqrt(d / Math.PI);
+    console.log(data);
+
+    var bubbles = special.selectAll("circle").data(data);
+    bubbles.enter().append(function (d, i) {
+	var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circ.setAttribute("cx", i * 50 + 25);
+	circ.setAttribute("cy", 50);
+	return circ;
+    }).attr("r", function (area) {
+	return "" + Math.sqrt(area / Math.PI);
     });
 }());
