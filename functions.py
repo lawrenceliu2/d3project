ffbc2ea2252dda002f6636f3cc08016c7f352c3c
cdelfau <cdelfau@stuy.edu>
2017-04-03 14:04:15 -0400

    still working on race percent

--- functions.py
+++ functions.py
@@ -4,28 +4,28 @@ n = open('csv/nyc.csv')
 nyc = csv.reader(n)
 
 d = open('csv/nycdoe.csv')
-doe = csv.reader(nd)
+doe = csv.reader(d)
 
 h = open('csv/specializedhs.csv')
 hs = csv.reader(h)
 
 def racepercent(borough, year):
-    races = []
-    for row in nyc:
-        if (row[0] == borough && row[1] == year) {
+    races = {}
+    for row in nyc[1:]:
+        print row
+        if (row[0] == borough and row[1] == year):
             population = row[2] * 1.0
             white = row[3] / population
-            black = row[4] / population
-            asian = row[5] / population
-            other = row[6] / population
-            hispanic = row[7] / population
-            foreign = row[8] / population
-            races = {"white": white, "black": black, "asian": asian, "other": other, "hispanic": hispanic, "foreign": foreign}
-        }
-        else
-            row++
+            black = row[5] / population #do not be alarmed! I KNOW HOW TO COUNT!
+            asian = row[6] / population
+            other = row[7] / population
+            hispanic = row[8] / population
+            foreign = row[9] / population
+            races = {"population": population, "white": white, "black": black, "asian": asian, "other": other, "hispanic": hispanic, "foreign": foreign}
     return races
-        
+
+p = racepercent(nyc, 1900)
+print p        
 #def genderpercent(borough, year):
 #    for row in ny
 
