7f46d2682e7d47945485e8eab13d687960393fa0
Wilson Berkow <wilsonberkow@gmail.com>
2017-04-04 21:49:53 -0400

    Implement rows of bubbles for each category (still random data)

--- static/bubbles.js
+++ static/bubbles.js
@@ -2,53 +2,109 @@ window.onload = function () {
     "use strict";
     // Use http://www.w3.org/2000/svg for svg elements
 
-    let special = d3.select("#slate-special");
+    const DEMOGRAPHICS = ["asian", "black", "hispanic", "foreign", "other"];
 
-    let data = [];
-    for (let yearI = 0; yearI < 100; yearI += 1) {
-        let yearData = [Math.random() * 100];
-        for (let i = 1; i < 9; i += 1) {
-            yearData[i] = yearData[i - 1] + Math.random() * 100;
-        }
-        data.push(yearData);
-    }
+    /* Struture of data:
+     * [ // array of years
+     *    { // data for first year
+     *        "nyc": {"asian": <percentage>, "black": <percentage>, ...},
+     *        "stuy": ...,
+     *        "bronx": ...,
+     *        ...
+     *    },
+     *    { // data for second year
+     *        ...
+     *    }
+     * ]
+     */
 
-    let slider = document.getElementById("yearInput");
-
-    let currentYearIndex = function () {
-        return +slider.value - (+slider.getAttribute("min"));
-    };
-
-    let circData = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    let bubbles = special.selectAll("circle").data(circData);
-    let circles = bubbles.enter().append(function (schoolIndex) {
-        var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circ.setAttribute("cx", schoolIndex * 50 + 25);
-        circ.setAttribute("cy", 50);
-        return circ;
-    });
-
-    let updateCircles = function () {
-        circles.transition().attr("r", function (schoolIndex) {
-            let yr = data[currentYearIndex()];
-            return "" + Math.sqrt(yr[schoolIndex] / Math.PI);
+    // Take an <svg> d3 selection, return a .append() of SVG circles to that <svg>.
+    let setupRow = function (svg) {
+        // The data for each circle is the name of the demographic;
+        // when deciding the radius of the circle we look up the percentage
+        // that the bubble represents, in our model (data).
+        return svg.selectAll("circle").data(DEMOGRAPHICS).enter().append(function (demographic, i) {
+            var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            circ.setAttribute("cx", i * 50 + 25);
+            circ.setAttribute("cy", 50);
+            return circ;
         });
     };
 
-    updateCircles();
+    // Params: `circles` is a d3 .append() of <circle>s to an <svg>
+    // Updates it with circles of the proper sizes based on the given demographic data
+    let updateRow = function (circles, demoData) {
+        DEMOGRAPHICS.forEach(function (key) {
+            circles.transition().attr("r", function (demo) {
+                // Consider demoData[demo] (the percentage) to be the
+                // area of the circle, and work back to get the radius
+                return "" + Math.sqrt(demoData[demo] / Math.PI);
+            });
+        });
+    };
+
+    let svgs = {
+        "nyc": d3.select("#slate-nyc"),
+        "stuy": d3.select("#slate-stuy"),
+        "bronx": d3.select("#slate-bronx"),
+        "tech": d3.select("#slate-tech"),
+    };
+    const ROW_CATEGORIES = Object.keys(svgs);
+
+    let circleControllers = {};
+    ROW_CATEGORIES.forEach(function (key) {
+        circleControllers[key] = setupRow(svgs[key]);
+    });
+
+    let yearSlider = document.getElementById("yearInput");
+    let minYr = +yearSlider.getAttribute("min");
+
+    // Generate random data:
+    let years = (function () {
+        let maxYr = +yearSlider.getAttribute("max");
+        let result = [];
+        let randomDemoData = function () {
+            let demos = {};
+            DEMOGRAPHICS.forEach(function (key) {
+                demos[key] = Math.random() * 80;
+            });
+            return demos;
+        };
+        for (let yearI = 0; yearI < maxYr - minYr; yearI += 1) {
+            let yearData = {};
+            // Make random demographic datasets for each category of row
+            ROW_CATEGORIES.forEach(function (category) {
+                yearData[category] = randomDemoData();
+            });
+            result.push(yearData);
+        }
+        return result;
+    }());
+
+    let getCurrentYearRows = function () {
+        return years[+yearSlider.value - minYr];
+    };
+
+    let updateAll = function () {
+        ROW_CATEGORIES.forEach(function (category) {
+            updateRow(circleControllers[category], getCurrentYearRows()[category]);
+        });
+    };
+
+    updateAll();
 
     // By default assume "input" event is not handled (as in IE 10).
     // `oninput` triggers as sliders slides, while `onchange` triggers
-    // only when the slider stops, so `oninput` is preferred.
+    // only when the yearSlider stops, so `oninput` is preferred.
     let inputEventTriggers = false;
 
-    slider.addEventListener("input", function () {
+    yearSlider.addEventListener("input", function () {
         inputEventTriggers = true;
-        updateCircles();
+        updateAll();
     });
-    slider.addEventListener("change", function () {
+    yearSlider.addEventListener("change", function () {
         if (!inputEventTriggers) {
-            updateCircles();
+            updateAll();
         }
     });
 };
