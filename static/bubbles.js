1c0cd169d9a67c85ad32abefc8a817f6dd18bab4
Wilson Berkow <wilsonberkow@gmail.com>
2017-04-04 20:47:55 -0400

    Normalize formatting of bubbles.js

--- static/bubbles.js
+++ static/bubbles.js
@@ -6,33 +6,33 @@ window.onload = function () {
 
     let data = [];
     for (let yearI = 0; yearI < 100; yearI += 1) {
-	let yearData = [Math.random() * 100];
-	for (let i = 1; i < 9; i += 1) {
-	    yearData[i] = yearData[i - 1] + Math.random() * 100;
-	}
-	data.push(yearData);
+        let yearData = [Math.random() * 100];
+        for (let i = 1; i < 9; i += 1) {
+            yearData[i] = yearData[i - 1] + Math.random() * 100;
+        }
+        data.push(yearData);
     }
 
     let slider = document.getElementById("yearInput");
 
     let currentYearIndex = function () {
-	return +slider.value - (+slider.getAttribute("min"));
+        return +slider.value - (+slider.getAttribute("min"));
     };
 
     let circData = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let bubbles = special.selectAll("circle").data(circData);
     let circles = bubbles.enter().append(function (schoolIndex) {
-	var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	circ.setAttribute("cx", schoolIndex * 50 + 25);
-	circ.setAttribute("cy", 50);
-	return circ;
+        var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circ.setAttribute("cx", schoolIndex * 50 + 25);
+        circ.setAttribute("cy", 50);
+        return circ;
     });
 
     let updateCircles = function () {
-	circles.transition().attr("r", function (schoolIndex) {
-	    let yr = data[currentYearIndex()];
-	    return "" + Math.sqrt(yr[schoolIndex] / Math.PI);
-	});
+        circles.transition().attr("r", function (schoolIndex) {
+            let yr = data[currentYearIndex()];
+            return "" + Math.sqrt(yr[schoolIndex] / Math.PI);
+        });
     };
 
     updateCircles();
@@ -43,12 +43,12 @@ window.onload = function () {
     let inputEventTriggers = false;
 
     slider.addEventListener("input", function () {
-	inputEventTriggers = true;
-	updateCircles();
+        inputEventTriggers = true;
+        updateCircles();
     });
     slider.addEventListener("change", function () {
-	if (!inputEventTriggers) {
-	    updateCircles();
-	}
+        if (!inputEventTriggers) {
+            updateCircles();
+        }
     });
 };
