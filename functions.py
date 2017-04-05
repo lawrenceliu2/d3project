56116df2ab7e63362c019656ec34468ce273994d
cdelfau <cdelfau@stuy.edu>
2017-04-04 23:52:15 -0400

    i need help debugging this first function :(

--- functions.py
+++ functions.py
@@ -1,34 +1,42 @@
 import csv
 
-n = open('csv/nyc.csv', 'r')
-nyc = n.readlines()
-nyc_list = list(nyc)
+#nyc data
+with open('csv/nyc.csv', 'rb') as n:
+    reader = csv.reader(n)
+    nyc_list = list(reader)
 #print nyc_list
 
-d = open('csv/nycdoe.csv', 'r')
-doe = d.readlines()
+#nyc doe data
+with open('csv/nycdoe.csv', 'rb') as d:
+    reader = csv.reader(d)
+    doe_list = list(reader)
+#print doe_list
 
-h = open('csv/specializedhs.csv', 'r')
-hs = h.readlines()
+#nyc doe specialized high school data
+with open('csv/specializedhs.csv', 'rb') as hs:
+    reader = csv.reader(hs)
+    hs_list = list(reader)
+#print hs_list
 
 def racepercent(borough, year):
-    races = {}
-    for row in nyc[1:]:
-        if (row[0] == borough and row[1] == year):
-            population = row[2] * 1.0
-            white = row[3] / population
-            black = row[5] / population #do not be alarmed! I KNOW HOW TO COUNT!
-            asian = row[6] / population
-            other = row[7] / population
-            hispanic = row[8] / population
-            foreign = row[9] / population
-            print "hello"
-            races = {"population": population, "white": white, "black": black, "asian": asian, "other": other, "hispanic": hispanic, "foreign": foreign}
+    races = dict()
+    for row in nyc_list[1:]:
+        for n in range(len(row)):
+            if row[0] == borough and row[1] == year:
+                population = row[2] * 1.0
+                white = row[3] / population
+                black = row[5] / population #do not be alarmed! I KNOW HOW TO COUNT!
+                asian = row[6] / population
+                other = row[7] / population
+                hispanic = row[8] / population
+                foreign = row[9] / population
+                races.update({'population': population, 'white': white, 'black': black, 'asian': asian, "other": other, 'hispanic': hispanic, 'foreign': foreign})
     return races
-
-p = racepercent(nyc, 1900)
+##THIS IS WHERE I NEED HELP IM NOT SURE WHY IT WONT ADD THE DATA TO THE DICT
+p = racepercent('manhattan', 1900)
 print p.keys()
 
+##IGNORE THIS FOR NOW ONCE THE FIRST FUNCITON WORKS, THESE WILL ALL BE VERY SIMILAR
 def doe_genderpercent(borough, year):
     genders = {}
     for row in doe[1:]:
