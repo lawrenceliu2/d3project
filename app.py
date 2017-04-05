6b4d8a6f6bbb098402afdf7110fc16e9ea3b8b44
OwO what's this..? <fortune.soleil@gmail.com>
2017-04-05 00:31:58 -0400

    attempted prematurely linking data

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html", data = ["data goes here"])
 
 if (__name__ == "__main__"):
     app.debug = False
